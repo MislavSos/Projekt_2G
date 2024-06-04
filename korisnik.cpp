4672912e3e5ecba64e5fe5f39cd8854319157535
JanJagustovic <jan.jagustovic@gmail.com>
2024-06-04 20:04:52 +0200

    p

--- korisnik.cpp
+++ korisnik.cpp
@@ -3,17 +3,18 @@
 #include <string>
 using namespace std;
 
-int main()
+/*int main()
 {
     cout << "+NOVI KORISNIK\t\tLOG IN" << endl;
     int odabir;
-    fstream korisnik;
+
     string unos_korisnika, ispis_datoteke;
     cout << "Ukoliko zelite napraviti novi racun unesite broj 1, a ako se zelite ulogirati u vec postojeci racun unesite broj 2."<<endl;
     cin >> odabir;
     if(odabir == 1)
     {
-        korisnik.open("korisnik.txt", ios::app);
+        fstream korisnik;
+        korisnik.open("korisnik.txt");
         cout << "Unesite ime racuna: ";
         getline(cin, unos_korisnika);
         cin.ignore();
@@ -22,6 +23,7 @@ int main()
     }
     if(odabir == 2)
     {
+        fstream korisnik;
         korisnik.open("korisnik.txt");
         while(getline(korisnik, ispis_datoteke))
         {
@@ -30,4 +32,20 @@ int main()
         korisnik.close();
     }
     return 0;
+}*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    // Create and open a text file
+    ofstream MyFile("filename.txt");
+
+    // Write to the file
+    MyFile << "Files can be tricky, but it is fun enough!";
+
+    // Close the file
+    MyFile.close();
 }
