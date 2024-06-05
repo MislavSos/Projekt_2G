9cc2af014461707498b3f46c89c609c4fa1f8ed6
JanJagustovic <jan.jagustovic@gmail.com>
2024-06-05 22:17:53 +0200

    p

--- korisnik.cpp
+++ korisnik.cpp
@@ -1,51 +1,32 @@
 #include <iostream>
-#include<fstream>
-#include <string>
-using namespace std;
-
-/*int main()
-{
-    cout << "+NOVI KORISNIK\t\tLOG IN" << endl;
-    int odabir;
-
-    string unos_korisnika, ispis_datoteke;
-    cout << "Ukoliko zelite napraviti novi racun unesite broj 1, a ako se zelite ulogirati u vec postojeci racun unesite broj 2."<<endl;
-    cin >> odabir;
-    if(odabir == 1)
-    {
-        fstream korisnik;
-        korisnik.open("korisnik.txt");
-        cout << "Unesite ime racuna: ";
-        getline(cin, unos_korisnika);
-        cin.ignore();
-        korisnik << unos_korisnika;
-        korisnik.close();
-    }
-    if(odabir == 2)
-    {
-        fstream korisnik;
-        korisnik.open("korisnik.txt");
-        while(getline(korisnik, ispis_datoteke))
-        {
-            cout << ispis_datoteke << endl;
-        }
-        korisnik.close();
-    }
-    return 0;
-}*/
-
-#include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    // Create and open a text file
-    ofstream MyFile("filename.txt");
-
-    // Write to the file
-    MyFile << "Files can be tricky, but it is fun enough!";
-
-    // Close the file
-    MyFile.close();
+    cout << "+NOVI KORISNIK\t\tLOG IN" << endl;
+    int odabir;
+    string unos_korisnika, ispis_datoteke;
+    fstream korisnik;
+    cout << "Ukoliko zelite napraviti novi racun unesite broj 1, a ako se zelite ulogirati u vec postojeci racun unesite broj 2." << endl;
+    cin >> odabir;
+    if (odabir == 1)
+    {
+        cout << endl;
+        korisnik.open("korisnik.txt", ios::app);
+        cin.ignore();
+        cout << "Unesite ime racuna: ";
+        getline(cin, unos_korisnika);
+        korisnik << unos_korisnika << endl;
+        korisnik.close();
+    }
+    if (odabir == 2)
+    {
+        korisnik.open("korisnik.txt");
+        while (getline(korisnik, ispis_datoteke))
+            cout << ispis_datoteke << endl;
+        korisnik.close();
+    }
+    return 0;
 }
